56d2256d830f6c20e0fc2849a061255f9201a826
mostafa <nematpor46@gmail.com>
2023-10-27 16:27:26 +0330

    Declarative Rendering

--- src/tutorial/TutorialRepl.vue
+++ src/tutorial/TutorialRepl.vue
@@ -124,7 +124,7 @@ updateExample()
       <div class="vt-doc" v-html="currentDescription"></div>
       <div class="hint" v-if="data[currentStep]?._hint">
         <button @click="toggleResult">
-          {{ showingHint ? 'Reset' : 'Show me!' }}
+          {{ showingHint ? 'بازنشانی' : 'به من نشان بده!' }}
         </button>
       </div>
       <footer>
