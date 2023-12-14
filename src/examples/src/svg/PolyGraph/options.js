93c1dfe52b6bf27edafd331a632dbbe77d9489c6
Yasha <85296952+the-pesar@users.noreply.github.com>
2023-12-14 21:54:36 +0330

    fix: correct typo options.js

--- src/examples/src/svg/PolyGraph/options.js
+++ src/examples/src/svg/PolyGraph/options.js
@@ -9,7 +9,7 @@ export default {
     stats: Array
   },
   computed: {
-    // computed یک پراپرتی برای نقاط چند ظلعی
+    // computed یک پراپرتی برای نقاط چند ضلعی
     points() {
       const total = this.stats.length
       return this.stats
